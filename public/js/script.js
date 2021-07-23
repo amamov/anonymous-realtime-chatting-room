dcafdbaced6ed60857dcffbe58d7e7b5af4a147f
amamov <amamov@kakao.com>
2021-07-23 12:14:55 +0900

    [update] using tailwindcss

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,28 @@ const getElementById = (id) => document.getElementById(id) || null;
 const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
-const inputElement = getElementById('chat_message_input');
 
 //* draw functions
 const drawHelloStranger = (username) =>
   (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
-const drawNewChat = (message) => {
-  const messageElement = document.createElement('div');
-  messageElement.innerText = message;
-  chattingBoxElement.append(messageElement);
+const drawNewChat = (message, isMe = false) => {
+  const wrapperChatBox = document.createElement('div');
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  else
+    chatBox = `
+    <div class='bg-green-300 w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
 
 //* global socket handler
@@ -30,14 +43,13 @@ const handleSubmit = (event) => {
   if (inputValue !== '') {
     event.target.elements[0].value = '';
     socket.emit('submit_chat', inputValue);
-    drawNewChat(`me : ${inputValue}`);
+    drawNewChat(`me : ${inputValue}`, true);
   }
 };
 
 //* main logic
 function registerEventListener() {
   formElement.addEventListener('submit', handleSubmit);
-  inputElement.addEventListener('click', () => {});
 }
 
 function helloUser() {
